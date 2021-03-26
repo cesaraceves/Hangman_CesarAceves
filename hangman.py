1e677c40871bc31c80189126cd4d390a8d3e6808
cesaraceves <79248805+cesaraceves@users.noreply.github.com>
2021-03-25 19:15:37 -0700

    Add files via upload

--- hangman.py
+++ hangman.py
@@ -1,23 +1,62 @@
-# Import modules to use
-from words import palabras
-from colorama import Fore,Style, init
-init()
+from words import palabras, vidas_pic
 import random
+import colorama #c
+from colorama import Fore,init#c
+init()#c
+colors = list(vars(colorama.Fore).values())#c paleta de colores
 
+vidas = 0
 
-print("Welcome to the game hangman in Python!! ")
+def good_word(words):
+        word = random.choice(words)
+        while '-' in word or ' ' in word:
+            word = random.choice(words)
+        return word
 
-def key_word(palabras):
-  word = random.choice(palabras)
+print(Fore.CYAN +"\nBienvenido al juego del ahorcado: (;")
+print(Fore.CYAN +"Esta es la palabra a adivinar\n")
+word = good_word(palabras).upper()
+word_list = list(word) #separa las letras en una lista
+count_letters = len(word_list) #cantidad de letras a adivinar
 
-  while '-' in word or ' ' in word: 
-    word = random.choice(palabras)
-  return word
+while count_letters > 0 or vidas == 6:
+    print(Fore.GREEN +"Adivina la palabra o frase")
 
+    print(word_list)
+    for i in word_list:
+        if i.isupper():
+            #print(" _ ",end="")
+            colored_chars = [random.choice(colors)+ " _ " ]#random color
+            print(" ".join(colored_chars),end="") #c 
+            
+        else:
+            print(i,end="")
 
-for i in key_word(palabras):
-    print(Fore.RED + " _ ",end="")
-print("")
+    print("")
 
+    letter = input("\nPalabra: ").upper()
+    if letter.isalpha() and len(letter) == 1:
+        con = 0
+        for i in range(len(word_list)):          
+            if letter == word_list[i]:
+                word_list[i] = letter.lower()
+                con = con + 1
+        if con == 0:
+            vidas = vidas + 1 
+        else:                
+            count_letters = count_letters - con #letra menos           
+            #print("Bien!, te faltan solo ",count_letters)
 
-
+        print(Fore.CYAN + vidas_pic[vidas])
+        if vidas == 6:
+            print(Fore.RED +"¡Has perdido!")
+            print("La palabra era: ", word)
+            break
+            
+        if count_letters == 0:
+            print(Fore.GREEN +"¡Enhorabuena!, has ganado el juego del ahorcado")
+            print(Fore.CYAN +"Palabra: ", word)
+            
+    else:
+        print(Fore.YELLOW +"No reconoce este valor, intenta de nuevo")  
+        print(Fore.YELLOW +"Aviso, solo puedes ingresar una letra")
